a7820b42a2e739564752a30e86c5b09848a0dce1
Nemanja Markovic <necamark@gmail.com>
2025-06-03 00:38:56 +0200

    Add getProductById method using Optional and return ResponseEntity with proper status

--- backend/SpringEcom/src/main/java/com/nemanja/SpringEcom/service/ProductService.java
+++ backend/SpringEcom/src/main/java/com/nemanja/SpringEcom/service/ProductService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -18,5 +19,9 @@ public class ProductService {
         return productRepo.findAll();
     }
 
+
+    public Optional<Product> getProductById(int id) {
+        return productRepo.findById(id);
+    }
 
 }
